d637bcd418a89718dc06813f6d9c8114b0450725
Tony Li <tli689@uwo.ca>
2024-10-31 15:13:59 -0400

    deck class

--- Deck.java
+++ Deck.java
@@ -1,6 +1,36 @@
 package hupoker;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+    // fields
+    private List<Card> deck;
 
+    // constructor
+    public Deck() {
+        deck = new ArrayList<>();
+        for (Card.Suit suit : Card.Suit.values()){
+            for (int i = 1; i <= 13; i++) {
+                deck.add(new Card(i, suit));
+            }
+        }
+    }
+    public void shuffle() {
+        Collections.shuffle(deck);
+    }
 
+    public Card dealCard() {
+        if (!deck.isEmpty()) {
+            return deck.remove(deck.size() - 1);
+        }
+        else return null;
+        }
+
+    public boolean isEmpty(){
+        return deck.isEmpty();
+    }
 }
+
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Standard heads up poker game built using java and javaswing (in progress). 
+Standard heads up poker game built using java and java swing (in progress). 
 Cards are dealt to each player from a 52 card deck and standard NLHE rules apply. Player will face a bot that will 
 be built using simple artificial intelligence logic. Feel free to play whenever you want to gamble without using
 real money! :)
